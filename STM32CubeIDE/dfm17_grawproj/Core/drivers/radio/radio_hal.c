65a86250d994e9f664d194d1642f56938a54a6b4
Derek Rowland <gx1400@gmail.com>
2023-02-16 20:19:53 -0600

    removing more macro expansion from radio_hal.h

--- STM32CubeIDE/dfm17_grawproj/Core/drivers/radio/radio_hal.c
+++ STM32CubeIDE/dfm17_grawproj/Core/drivers/radio/radio_hal.c
@@ -81,97 +81,32 @@ void radio_hal_SpiReadData(uint8_t byteCount, uint8_t* pData)
   SpiReadData(byteCount, pData);
 }
 
-#ifdef PLATFORM_GRAW_DFM17
-
-
-
-#endif
-
 #ifdef RADIO_DRIVER_EXTENDED_SUPPORT
-BIT radio_hal_Gpio0Level(void)
+unsigned char radio_hal_Gpio0Level(void)
 {
-  BIT retVal = FALSE;
-
-#ifdef SILABS_PLATFORM_DKMB
-  retVal = FALSE;
-#endif
-#ifdef SILABS_PLATFORM_UDP
-  retVal = EZRP_RX_DOUT;
-#endif
-#if (defined SILABS_PLATFORM_RFSTICK) || (defined SILABS_PLATFORM_LCDBB)
-  retVal = RF_GPIO0;
-#endif
-#if (defined SILABS_PLATFORM_WMB930)
-  retVal = FALSE;
-#endif
-#if defined (SILABS_PLATFORM_WMB912)
-  #ifdef SILABS_IO_WITH_EXTENDER
-    //TODO
-    retVal = FALSE;
-  #endif
-#endif
+	unsigned char retVal = 0;
 
   return retVal;
 }
 
-BIT radio_hal_Gpio1Level(void)
+unsigned char radio_hal_Gpio1Level(void)
 {
-  BIT retVal = FALSE;
+	unsigned char retVal = 0;
+  return retVal;
+}
 
-#ifdef SILABS_PLATFORM_DKMB
-  retVal = FSK_CLK_OUT;
-#endif
-#ifdef SILABS_PLATFORM_UDP
-  retVal = FALSE; //No Pop
-#endif
-#if (defined SILABS_PLATFORM_RFSTICK) || (defined SILABS_PLATFORM_LCDBB) || (defined SILABS_PLATFORM_WMB930)
-  retVal = RF_GPIO1;
-#endif
-#if defined (SILABS_PLATFORM_WMB912)
-  #ifdef SILABS_IO_WITH_EXTENDER
-    //TODO
-    retVal = FALSE;
-  #endif
-#endif
+unsigned char radio_hal_Gpio2Level(void)
+{
+	unsigned char retVal = 0;
 
   return retVal;
 }
 
-BIT radio_hal_Gpio2Level(void)
+unsigned char radio_hal_Gpio3Level(void)
 {
-  BIT retVal = FALSE;
+	unsigned char retVal = 0;
 
-#ifdef SILABS_PLATFORM_DKMB
-  retVal = DATA_NFFS;
-#endif
-#ifdef SILABS_PLATFORM_UDP
-  retVal = FALSE; //No Pop
-#endif
-#if (defined SILABS_PLATFORM_RFSTICK) || (defined SILABS_PLATFORM_LCDBB) || (defined SILABS_PLATFORM_WMB930)
-  retVal = RF_GPIO2;
-#endif
-#if defined (SILABS_PLATFORM_WMB912)
-  #ifdef SILABS_IO_WITH_EXTENDER
-    //TODO
-    retVal = FALSE;
-  #endif
-#endif
 
-  return retVal;
-}
-
-BIT radio_hal_Gpio3Level(void)
-{
-  BIT retVal = FALSE;
-
-#if (defined SILABS_PLATFORM_RFSTICK) || (defined SILABS_PLATFORM_LCDBB) || (defined SILABS_PLATFORM_WMB930)
-  retVal = RF_GPIO3;
-#elif defined (SILABS_PLATFORM_WMB912)
-  #ifdef SILABS_IO_WITH_EXTENDER
-    //TODO
-    retVal = FALSE;
-  #endif
-#endif
 
   return retVal;
 }
